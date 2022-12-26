37d21ab117b2f3b8afc2dc1f86650c56fce6e9fa
Umid1982 <umidurinov14@gmail.com>
2022-12-26 21:01:58 +0500

    After a like, do not display posts on the page

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -46,8 +46,13 @@ class UserController extends Controller
     public function followingPost()
     {
         $followedIds = auth()->user()->followings()->latest()->get()->pluck('id')->toArray();
-        $posts = Post::whereIn('user_id', $followedIds)->get();
-        $posts = $this->prepareLikedPosts($posts);
+
+        $likedPostIds = LikedPost::where('user_id', auth()->id())
+            ->get('post_id')->pluck('post_id')->toArray();
+//        После лайка не отображать посты на странице
+        $posts = Post::whereIn('user_id', $followedIds)
+            ->whereNotIn('id',$likedPostIds)->get();
+
         return PostResource::collection($posts);
     }
 
